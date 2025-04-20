600606c1a10c6e5a9f7236ce268b8e40799b8935
ztsia <leosia929@gmail.com>
2025-04-20 16:37:57 +0800

    removed unnecessary files

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -17,7 +17,7 @@ class ItemSeeder extends Seeder
         Item::create([
             'name' => 'Indoor Theme Park Ticket',
             'description' => 'Enjoy a day of fun and excitement at our indoor theme park!',
-            'image' => 'images/items/indoor.jpg', // Default image path
+            'image' => 'images/items/indoor.jpg', 
             'normalPrice' => 50.00, // Normal price
             'childrenSeniorPrice' => 30.00, // Children/Senior price
             'studentPrice' => 40.00, // Student price
@@ -25,7 +25,7 @@ class ItemSeeder extends Seeder
         Item::create([
             'name' => 'Outdoor Theme Park Ticket',
             'description' => 'Experience the thrill of our outdoor theme park rides!',
-            'image' => 'images/items/outdoor.jpg', // Default image path
+            'image' => 'images/items/outdoor.jpg', 
             'normalPrice' => 60.00, // Normal price
             'childrenSeniorPrice' => 35.00, // Children/Senior price
             'studentPrice' => 45.00, // Student price
@@ -33,7 +33,7 @@ class ItemSeeder extends Seeder
         Item::create([
             'name' => 'Water Park Ticket',
             'description' => 'Beat the heat with a day at our water park!',
-            'image' => 'images/items/waterpark.jpg', // Default image path
+            'image' => 'images/items/waterpark.jpg', 
             'normalPrice' => 55.00, // Normal price
             'childrenSeniorPrice' => 32.00, // Children/Senior price
             'studentPrice' => 42.00, // Student price
